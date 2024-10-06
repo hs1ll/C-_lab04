fa21094f055bb0f2518a63223903aaa7fea8e999
hs1ll <oleyn.alexander@gmail.com>
2024-10-06 11:50:18 +0300

    make enums in internal file

---
+++ Task06Settings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task06Settings
+{
+    enum FIGURES
+    {
+        Rectangle,
+        Ellipse,
+    }
+
+    enum FIGURE_NUMBER
+    {
+        Single = 1,
+        Pair = 2,
+    }
+
+    enum INTERVALS
+    {
+        Slow = 3000,
+        Medium = 1000,
+        Fast = 100,
+    }
+}
